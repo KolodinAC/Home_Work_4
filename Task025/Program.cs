6dad8734984169c97534412803372b9103076b56
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-05 09:55:24 +0300

    Task025 added

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа (A и B) и 
+// метод который возводит число A в натуральную степень B.
+
+double numbA = UserInputA();
+double numbB = UserInputB();
+double result = RiseNumbToPow(numbA, numbA);
+Console.WriteLine(result);
+
+
+// Методы:
+
+double UserInputA()
+{
+    Console.WriteLine("Введите первое число:");
+    bool isNum = double.TryParse(Console.ReadLine(), out double num);
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
+
+double UserInputB()
+{
+    Console.WriteLine("Введите второе число:");
+    bool isNum = double.TryParse(Console.ReadLine(), out double num);
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
+
+double RiseNumbToPow(double numbA, double numB)
+{
+    double num = numbA;
+    double pow = numB;
+    double res = 0;
+    
+    for (int i = 1; i <= pow; i++)
+    {
+        res = res * num;
+    }
+    return res;    
+}
